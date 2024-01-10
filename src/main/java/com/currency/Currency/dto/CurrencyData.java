3ddbf98989a70581cd76d3ce762fd0dc948b83ea
Von__Carstein <yurij.yashenev.00@gmail.com>
2024-01-10 07:11:29 +0300

    first take commit

---
+++ src/main/java/com/currency/Currency/dto/CurrencyData.java
@@ -0,0 +1,12 @@
+package com.currency.Currency.dto;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class CurrencyData {
+    private LocalDate date;
+    private double usd;
+    private double eur;
+}
